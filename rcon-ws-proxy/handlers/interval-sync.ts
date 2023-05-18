cba38c096d4127b44abf277bc340c1dc6ec179b6
jalho <alho.jere@gmail.com>
2023-05-18 18:18:03 +0300

    Trace connections

--- rcon-ws-proxy/handlers/interval-sync.ts
+++ rcon-ws-proxy/handlers/interval-sync.ts
@@ -1,13 +1,17 @@
-import type WebSocket from "ws";
+import Log4js from "log4js";
+import { TConnections, TIdentifiedSocket } from "../main.js";
 
-const syncRcon = (downstream: WebSocket.WebSocket) => () => {
+const syncRcon = (downstream: TIdentifiedSocket, logger: Log4js.Logger, connections: TConnections) => () => {
   const payload: IAdminUIRemoteState = {
     lastSyncTsMs: Date.now(),
     players: {},
     tcs: {},
   };
 
+  logger.trace("Sending RCON sync payload to client");
   downstream.send(JSON.stringify(payload));
+
+  // TODO: update connections[clientId].lastAck when client acks
 };
 
 export default syncRcon;
--- rcon-ws-proxy/main.ts
+++ rcon-ws-proxy/main.ts
@@ -1,9 +1,15 @@
 /// <reference types="../interfaces/typescript/admin-ui.d.ts" />
 
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 import log4js from "log4js";
+import crypto from "node:crypto";
 import messageHandler from "./handlers/message.js";
 import syncRcon from "./handlers/interval-sync.js";
+
+const config = {
+  rconSyncIntervalMs: 3000,
+  wsListenPort: 8002,
+};
 
 const OPS = {
   "check health": () => {
@@ -14,20 +20,40 @@ export type TOPS = typeof OPS;
 
 const logger = log4js
   .configure({
-    appenders: { stdout: { type: "stdout" } },
+    appenders: { stdout: { type: "stdout", layout: { pattern: "%[[%d] %p %X{clientId}%] - %m", type: "pattern" } } },
     categories: { default: { appenders: ["stdout"], level: "all" } },
   })
   .getLogger();
-const wss = new WebSocketServer({ port: 8002 });
+const wss = new WebSocketServer({ port: config.wsListenPort });
+
+const connections: { [clientId: string]: { socket: WebSocket; lastAck: number } } = {};
+export type TConnections = typeof connections;
+export type TIdentifiedSocket = WebSocket & { clientId: string };
 
 wss.on("connection", function connection(downstream) {
+  const logger = log4js.getLogger();
+  const clientId = crypto.randomUUID();
+  logger.addContext("clientId", clientId);
+
+  if (connections[clientId] === undefined) connections[clientId] = { socket: downstream, lastAck: Date.now() };
+  else throw new Error(`Duplicate client ID ${clientId} detected! This is a bug!`);
+
+  logger.info("%d clients connected in total", wss.clients.size);
+
   downstream.on("error", logger.error);
-
   downstream.on("message", (data) => messageHandler(data, downstream, logger, OPS));
+  const identifiedDownstream = downstream as TIdentifiedSocket;
+  identifiedDownstream.clientId = clientId;
 
-  setInterval(syncRcon(downstream), 1000);
+  setInterval(syncRcon(identifiedDownstream, logger, connections), config.rconSyncIntervalMs);
+
+  // TODO: prune dead connections based on lastAck in regular interval
 });
 
 wss.on("listening", () => {
-  logger.info("Listening %s", Object.values(wss.address()).join(":"));
+  logger.info(
+    "Listening %s. Syncing RCON every %d ms for every connected client.",
+    Object.values(wss.address()).join(":"),
+    config.rconSyncIntervalMs
+  );
 });
