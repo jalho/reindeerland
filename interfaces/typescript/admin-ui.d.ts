12d827d31667f9d210d680e1a3eef5668673063b
jalho <alho.jere@gmail.com>
2023-05-18 17:33:24 +0300

    Use TypeScript

--- interfaces/typescript/admin-ui.d.ts
+++ interfaces/typescript/admin-ui.d.ts
@@ -3,4 +3,7 @@ interface IAdminUIRemoteState {
    * The last time the remote state was synced, in milliseconds since the epoch.
    */
   lastSyncTsMs: number;
+
+  players: {},
+  tcs: {},
 }
--- rcon-ws-proxy/main.ts
+++ rcon-ws-proxy/main.ts
@@ -1,31 +1,39 @@
-import { WebSocketServer } from 'ws';
+/// <reference types="../interfaces/typescript/admin-ui.d.ts" />
+
+import { WebSocketServer } from "ws";
 import log4js from "log4js";
 
-const OPS = {
-  "get server name": () => {
-    return "Reindeerland Noob Friendly Weekly";
-  }
-}
+const OPS: Record<string, () => unknown> = {
+  "check health": () => {
+    return "I'm alive!";
+  },
+};
 
 const logger = log4js
   .configure({
     appenders: { stdout: { type: "stdout" } },
-    categories: { default: { appenders: ["stdout"], level: "all" } }
-  }).getLogger();
+    categories: { default: { appenders: ["stdout"], level: "all" } },
+  })
+  .getLogger();
 const wss = new WebSocketServer({ port: 8002 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', logger.error);
+wss.on("connection", function connection(ws) {
+  ws.on("error", logger.error);
 
-  ws.on('message', function message(data) {
-    logger.info('Received message', data);
+  ws.on("message", function message(data) {
+    logger.info("Received message", data);
 
     let json;
     try {
       json = JSON.parse(data.toString());
       if (!json.id) throw new Error('Expected "id" in message');
       if (!json.op) throw new Error('Expected "op" in message');
-      if (!OPS[json.op]) throw new Error(`Unknown operation "${json.op}"`);
+      if (!OPS[json.op])
+        throw new Error(
+          `Unknown operation "${json.op}". Supported operations: ${Object.keys(OPS)
+            .map((o) => `"${o}"`)
+            .join(", ")}`
+        );
     } catch (e) {
       logger.error(e);
       return;
@@ -35,20 +43,17 @@ wss.on('connection', function connection(ws) {
     const response = {
       payload: payloadConstructor(),
       id: json.id,
-    }
+    };
     ws.send(JSON.stringify(response));
-    logger.info('Sent message');
+    logger.info("Sent message");
   });
 
   setInterval(() => {
-    /**
-     * @type IAdminUIRemoteState
-     */
-    const payload = {
+    const payload: IAdminUIRemoteState = {
       lastSyncTsMs: Date.now(),
       players: {},
       tcs: {},
-    }
+    };
 
     ws.send(JSON.stringify(payload));
   }, 1000);
