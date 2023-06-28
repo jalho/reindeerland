334e8bb6021f8b0070a9bc11512d05690d250163
jalho <alho.jere@gmail.com>
2023-06-28 18:07:02 +0300

    Remove obsolete Upstream#send

--- admin-ui/state/Upstream.ts
+++ admin-ui/state/Upstream.ts
@@ -3,6 +3,9 @@
 const HEADERNAME_USERNAME: TAuthHeader = "x-rcon-ws-proxy-username";
 const HEADERNAME_PASSWORD: TAuthHeader = "x-rcon-ws-proxy-password";
 
+/**
+ * Credentials needed for connecting to upstream.
+ */
 export interface IConnectUpstreamCredentials {
   username: string;
   password: string;
@@ -60,61 +63,6 @@ class Upstream {
         resolve(s);
       });
       s.addEventListener("error", reject);
-    });
-  }
-
-  /**
-   * Send a message to the upstream and wait for a response.
-   *
-   * @param message Message to send
-   * @param id ID used to identify the response to the message sent
-   * @returns Data of the message event that was received as a response to the
-   *          message sent, or `null` if no response was received for whatever
-   *          reason.
-   */
-  public async send<T extends { id: string }>(message: T): Promise<Record<string, unknown> | null> {
-    const socket = await this.connect();
-    socket.send(JSON.stringify(message));
-    return new Promise((resolve) => {
-      socket.addEventListener("message", (event) => {
-        let json: Record<string, unknown>;
-        try {
-          const parsed = JSON.parse(event.data);
-          if (typeof parsed === "object" && parsed !== null) {
-            json = parsed;
-          } else {
-            throw new Error(`Expected upstream data be a JSON object, got "${typeof parsed}"`);
-          }
-          if (!("id" in json))
-            throw new Error(
-              `Expected upstream data to have "id" property, got only ${Object.keys(json)
-                .map((k) => `"${k}"`)
-                .join(", ")}`
-            );
-          if (!("payload" in json))
-            throw new Error(
-              `Expected upstream data to have "payload" property, got only ${Object.keys(json)
-                .map((k) => `"${k}"`)
-                .join(", ")}`
-            );
-        } catch (error) {
-          console.error("Got a message in invalid format!", error.message);
-          return resolve(null);
-        }
-
-        if (json.id !== message.id) {
-          const error = new Error(`Expected event.data.id to be "${message.id}", got "${json.id}"`);
-          console.error(error.message);
-          return resolve(null);
-        }
-
-        return resolve(json);
-      });
-
-      socket.addEventListener("error", (error) => {
-        console.error("WebSocket error:", error);
-        return resolve(null);
-      });
     });
   }
 }
