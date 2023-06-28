b8f39848c5f00b2297b6cd9e5eb2edd870947bbd
jalho <alho.jere@gmail.com>
2023-06-29 02:07:08 +0300

    Fix marker positioning

--- admin-ui/components/WorldTool.tsx
+++ admin-ui/components/WorldTool.tsx
@@ -6,9 +6,18 @@ interface IProps {
   upstream: URL;
 }
 
-const MAX_X = 1000;
-const ORIGIN = MAX_X / 2;
-const SCALE = MAX_X / 4500;
+/**
+ * When RustDedicated is started with worldSize 4500, the corresponding rendered
+ * map image is 5500 x 5500 px, and the coordinates RCON reports in various
+ * commands are relative to those dimensions. (Accurate as of Jun 2023)
+ */
+const GAMEWORLD_SIZE = 5500;
+/**
+ * Size of the map image element rendered on the web page.
+ */
+const MAP_ELEMENT_SIZE = 1000;
+const GAMEWORLD_ORIGIN = GAMEWORLD_SIZE / 2;
+const SCALE = MAP_ELEMENT_SIZE / GAMEWORLD_SIZE;
 const MARKER_RADIUS = 10;
 
 const WorldTool = (props: IProps): React.JSX.Element => {
@@ -17,7 +26,7 @@ const WorldTool = (props: IProps): React.JSX.Element => {
 
   return (
     <>
-      <div style={{ width: MAX_X, position: "relative" }}>
+      <div style={{ width: MAP_ELEMENT_SIZE, position: "relative" }}>
         <img
           src={protocol + "//" + host + pathname}
           alt="Map of Reindeerland"
@@ -33,8 +42,9 @@ const WorldTool = (props: IProps): React.JSX.Element => {
                 width: MARKER_RADIUS,
                 height: MARKER_RADIUS,
                 backgroundColor: "red",
-                left: SCALE * (ORIGIN + x),
-                top: SCALE * (ORIGIN - y),
+                left: SCALE * (GAMEWORLD_ORIGIN + x) - MARKER_RADIUS / 2,
+                top: SCALE * (GAMEWORLD_ORIGIN - y) - MARKER_RADIUS / 2,
+                borderRadius: "50%",
               }}
             />
           );
