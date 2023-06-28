bfb4f5ed7c42f7a9a7b3900b8f115bb5886dcec7
jalho <alho.jere@gmail.com>
2023-06-29 01:51:00 +0300

    Map players into markers

--- admin-ui/components/WorldTool.tsx
+++ admin-ui/components/WorldTool.tsx
@@ -1,32 +1,44 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { State } from "../state/store";
 
 interface IProps {
   upstream: URL;
 }
 
-// TODO: remove these
-const banditCamp = [850, 67, -1119.4] as const;
-const [x, z, y] = banditCamp;
+const MAX_X = 1000;
+const ORIGIN = MAX_X / 2;
+const SCALE = MAX_X / 4500;
+const MARKER_RADIUS = 10;
 
 const WorldTool = (props: IProps): React.JSX.Element => {
   const { protocol, host, pathname } = props.upstream;
+  const players = useSelector((state: State) => state.players);
 
   return (
     <>
-      <div style={{ width: "100%", position: "relative" }}>
+      <div style={{ width: MAX_X, position: "relative" }}>
         <img
           src={protocol + "//" + host + pathname}
           alt="Map of Reindeerland"
           style={{ width: "100%", position: "absolute" }}
         />
-        <div
-          style={{
-            position: "absolute",
-            width: 10,
-            height: 10,
-            backgroundColor: "red",
-          }}
-        />
+        {Object.values(players).map((p) => {
+          const [x, z, y] = p.position;
+          return (
+            <div
+              key={p.id}
+              style={{
+                position: "absolute",
+                width: MARKER_RADIUS,
+                height: MARKER_RADIUS,
+                backgroundColor: "red",
+                left: SCALE * (ORIGIN + x),
+                top: SCALE * (ORIGIN - y),
+              }}
+            />
+          );
+        })}
       </div>
       <p>TODO: Legends here.</p>
       <p>TODO: Controls here.</p>
