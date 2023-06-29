c668269e443391572b13f489cb8967a54ee7ddf5
jalho <alho.jere@gmail.com>
2023-06-29 18:37:01 +0300

    Add login

--- admin-ui/components/Dashboard.tsx
+++ admin-ui/components/Dashboard.tsx
@@ -11,9 +11,9 @@ import Paper from "@mui/material/Paper";
 import Playerlist from "./Playerlist";
 import WorldTool from "./WorldTool";
 import { MAP_UPSTREAM } from "../constants/upstreams";
-import { Dispatch, connectUpstream } from "../state/store";
-import { IConnectUpstreamCredentials } from "../state/Upstream";
-import { useDispatch } from "react-redux";
+import { State } from "../state/store";
+import { useSelector } from "react-redux";
+import Login from "./Login";
 
 const MAP_ELEMENT_SIZE = 500;
 
@@ -40,70 +40,65 @@ const AppBar = styled(MuiAppBar, {
 
 const defaultTheme = createTheme();
 
-// TODO: get as user input
-const credentials: IConnectUpstreamCredentials = {
-  password: "bar",
-  username: "foo",
-};
-
 export default function Dashboard() {
-  const dispatch = useDispatch<Dispatch>();
-
-  // TODO: dispatch when user has provided credentials
-  React.useEffect(() => {
-    dispatch(connectUpstream(credentials));
-  }, []);
+  const loggedIn = useSelector<State, boolean>((s) => {
+    return s.connected && s.lastSyncTsMs > 0;
+  });
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        <AppBar position="absolute">
-          <Toolbar
+      {!loggedIn ? (
+        <Login />
+      ) : (
+        <Box sx={{ display: "flex" }}>
+          <CssBaseline />
+          <AppBar position="absolute">
+            <Toolbar
+              sx={{
+                pr: "24px", // keep right padding when drawer closed
+              }}
+            >
+              <Typography component="h1" variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+                Reindeerland
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Box
+            component="main"
             sx={{
-              pr: "24px", // keep right padding when drawer closed
+              backgroundColor: (theme) =>
+                theme.palette.mode === "light" ? theme.palette.grey[100] : theme.palette.grey[900],
+              flexGrow: 1,
+              height: "100vh",
+              overflow: "auto",
             }}
           >
-            <Typography component="h1" variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-              Reindeerland
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === "light" ? theme.palette.grey[100] : theme.palette.grey[900],
-            flexGrow: 1,
-            height: "100vh",
-            overflow: "auto",
-          }}
-        >
-          <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3} justifyContent={"center"}>
-              {/* Map */}
-              <Grid item>
-                <Paper
-                  style={{
-                    width: MAP_ELEMENT_SIZE,
-                    height: MAP_ELEMENT_SIZE,
-                  }}
-                >
-                  <WorldTool upstream={MAP_UPSTREAM} mapElementSizePx={MAP_ELEMENT_SIZE} />
-                </Paper>
-              </Grid>
+            <Toolbar />
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+              <Grid container spacing={3} justifyContent={"center"}>
+                {/* Map */}
+                <Grid item>
+                  <Paper
+                    style={{
+                      width: MAP_ELEMENT_SIZE,
+                      height: MAP_ELEMENT_SIZE,
+                    }}
+                  >
+                    <WorldTool upstream={MAP_UPSTREAM} mapElementSizePx={MAP_ELEMENT_SIZE} />
+                  </Paper>
+                </Grid>
 
-              {/* Player list */}
-              <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-                  <Playerlist />
-                </Paper>
+                {/* Player list */}
+                <Grid item xs={12}>
+                  <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+                    <Playerlist />
+                  </Paper>
+                </Grid>
               </Grid>
-            </Grid>
-          </Container>
+            </Container>
+          </Box>
         </Box>
-      </Box>
+      )}
     </ThemeProvider>
   );
 }
---
+++ admin-ui/components/Login.tsx
@@ -0,0 +1,76 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { useDispatch } from "react-redux";
+import { Dispatch, connectUpstream } from "../state/store";
+import { IConnectUpstreamCredentials } from "../state/Upstream";
+
+export default function Login() {
+  const dispatch = useDispatch<Dispatch>();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const username = data.get("username");
+    if (typeof username !== "string") return;
+    const password = data.get("password");
+    if (typeof password !== "string") return;
+    const credentials: IConnectUpstreamCredentials = {
+      username,
+      password,
+    };
+    dispatch(connectUpstream(credentials));
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+            Enter
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
