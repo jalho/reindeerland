3fcaeb5ae5a4ded30896801ff197cc40f7ff2e37
jalho <alho.jere@gmail.com>
2023-06-29 15:39:17 +0300

    Add logger

--- Makefile
+++ Makefile
@@ -17,5 +17,6 @@ tls-proxy-image:
 	@docker build -t tls-proxy:local -f tls-proxy/Dockerfile tls-proxy
 
 player-detail-service-image:
+	@cd player-detail-service && npm install
 	@cd player-detail-service && npm run build
 	@docker build -t player-detail-service:local -f player-detail-service/Dockerfile player-detail-service
--- player-detail-service/main.ts
+++ player-detail-service/main.ts
@@ -1,8 +1,25 @@
 import http from "node:http";
+import fs from "node:fs";
+import log4js from "log4js";
 
-const [, , PORT] = process.argv;
+const logg = log4js
+  .configure({
+    appenders: {
+      stdout: {
+        type: "stdout",
+      },
+    },
+    categories: { default: { appenders: ["stdout"], level: "all" } },
+  })
+  .getLogger();
+
+const [, , PORT, STORE_FILE_PATH] = process.argv;
+fs.accessSync(STORE_FILE_PATH, fs.constants.R_OK);
+fs.accessSync(STORE_FILE_PATH, fs.constants.W_OK);
+logg.info("Store file '%s' is OK: readable & writeable", STORE_FILE_PATH);
+
 const s = http.createServer((i, o) => {
   console.log("[%s] [%s] [%s]", new Date(), i.method, i.url);
   o.end();
 });
-s.listen(PORT ?? 80, () => console.log("Listening %s", s.address()));
+s.listen(PORT ?? 80, () => logg.info("Listening %s", s.address()));
