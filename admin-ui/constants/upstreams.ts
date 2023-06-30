ddc4cce1de9d28bdfd5cbb044ace08d182425cfb
jalho <alho.jere@gmail.com>
2023-06-30 12:00:31 +0300

    Use port 28016 on weekly.reindeerland.eu VM

--- admin-ui/constants/upstreams.ts
+++ admin-ui/constants/upstreams.ts
@@ -1,6 +1,6 @@
 // map-service
-export const MAP_UPSTREAM = new URL("https://sandbox.reindeerland.eu/map");
+export const MAP_UPSTREAM = new URL("https://weekly.reindeerland.eu:28016/map");
 
 // rcon-ws-proxy
-export const RCON_UPSTREAM_LOGIN = new URL("https://sandbox.reindeerland.eu/login");
-export const RCON_UPSTREAM_WS = new URL("wss://sandbox.reindeerland.eu/ws");
+export const RCON_UPSTREAM_LOGIN = new URL("https://weekly.reindeerland.eu:28016/login");
+export const RCON_UPSTREAM_WS = new URL("wss://weekly.reindeerland.eu:28016/ws");
