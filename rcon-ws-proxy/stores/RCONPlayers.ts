7e520455e06b6940e8467a1fb1d59c1de093a697
jalho <alho.jere@gmail.com>
2023-07-01 22:47:05 +0300

    Add error boundary

--- rcon-ws-proxy/stores/RCONPlayers.ts
+++ rcon-ws-proxy/stores/RCONPlayers.ts
@@ -9,44 +9,49 @@ class RCONPlayers extends _RCON<IRCONPlayer> {
   private _countryCache: { [steamId: string]: string } = {};
 
   protected async sync(): Promise<void> {
-    // playerlistpos
-    const playerlistpos_response = await this.sendRconCommand("playerlistpos");
-    const playerlistpos_parsed = playerlistpos(playerlistpos_response.rconMessage);
+    try {
+      // playerlistpos
+      const playerlistpos_response = await this.sendRconCommand("playerlistpos");
+      const playerlistpos_parsed = playerlistpos(playerlistpos_response.rconMessage);
 
-    // playerlist
-    const playerlist_response = await this.sendRconCommand("playerlist");
-    const playerlist_parsed = playerlist(playerlist_response.rconMessage);
+      // playerlist
+      const playerlist_response = await this.sendRconCommand("playerlist");
+      const playerlist_parsed = playerlist(playerlist_response.rconMessage);
 
-    const online: { [id: string]: true } = {};
-    for (const player of playerlist_parsed) {
-      online[player.SteamID] = true;
-      const { POS, ROT } = playerlistpos_parsed[player.SteamID] ?? {};
-      const { Address, ConnectedSeconds, DisplayName, Health, Ping } = player;
-      const country = await this.resolveCountry(player.SteamID, Address);
+      const online: { [id: string]: true } = {};
+      for (const player of playerlist_parsed) {
+        online[player.SteamID] = true;
+        const { POS, ROT } = playerlistpos_parsed[player.SteamID] ?? {};
+        const { Address, ConnectedSeconds, DisplayName, Health, Ping } = player;
+        const country = await this.resolveCountry(player.SteamID, Address);
 
-      const rconPlayer: IRCONPlayer = {
-        connected_seconds: ConnectedSeconds,
-        country: country ?? "",
-        health: Health,
-        id: player.SteamID,
-        ip_address: Address,
-        name: DisplayName,
-        ping: Ping,
-        position: POS,
-        rotation: ROT,
-        online: true,
-      };
-      this._cache.set(player.SteamID, rconPlayer);
-    }
-
-    // set offline status for disconnected players
-    for (const [id, player] of this._cache) {
-      if (online[id]) continue;
-      else if (player.online) {
-        player.online = false;
-        this._cache.set(id, player);
-        this._logg.info("Marked player offline: %s (Steam ID %s)", player.name, id);
+        const rconPlayer: IRCONPlayer = {
+          connected_seconds: ConnectedSeconds,
+          country: country ?? "",
+          health: Health,
+          id: player.SteamID,
+          ip_address: Address,
+          name: DisplayName,
+          ping: Ping,
+          position: POS,
+          rotation: ROT,
+          online: true,
+        };
+        this._cache.set(player.SteamID, rconPlayer);
       }
+
+      // set offline status for disconnected players
+      for (const [id, player] of this._cache) {
+        if (online[id]) continue;
+        else if (player.online) {
+          player.online = false;
+          this._cache.set(id, player);
+          this._logg.info("Marked player offline: %s (Steam ID %s)", player.name, id);
+        }
+      }
+    } catch (e_sync) {
+      this._logg.error(e_sync);
+      return;
     }
   }
 
--- rcon-ws-proxy/stores/RCONToolcupboards.ts
+++ rcon-ws-proxy/stores/RCONToolcupboards.ts
@@ -6,35 +6,40 @@ import _Store from "./__Store.js";
 
 class RCONToolcupboards extends _RCON<IRCONToolCupboard> {
   protected async sync(): Promise<void> {
-    // listtoolcupboards
-    const listtoolcupboards_response = await this.sendRconCommand("listtoolcupboards");
-    const listtoolcupboards_parsed = listtoolcupboards(listtoolcupboards_response.rconMessage);
-
-    const online: { [id: string]: true } = {};
-    for (const [id, tc] of Object.entries(listtoolcupboards_parsed)) {
-      online[id] = true;
-      const rconTc: IRCONToolCupboard = {
-        authed_players_count: tc.Authed,
-        destroyed: false,
-        id,
-        position: tc.Position,
-      };
-      this._cache.set(id, rconTc);
-    }
-
-    // set destroyed status for TCs that are no longer on the map
-    for (const [id, tc] of this._cache) {
-      if (online[id]) continue;
-      else if (!tc.destroyed) {
-        tc.destroyed = true;
-        this._cache.set(id, tc);
-        this._logg.info(
-          "Marked TC at [%s] destroyed: entity ID %s, authed players %d",
-          tc.position.join(", "),
-          tc.id,
-          tc.authed_players_count
-        );
+    try {
+      // listtoolcupboards
+      const listtoolcupboards_response = await this.sendRconCommand("listtoolcupboards");
+      const listtoolcupboards_parsed = listtoolcupboards(listtoolcupboards_response.rconMessage);
+  
+      const online: { [id: string]: true } = {};
+      for (const [id, tc] of Object.entries(listtoolcupboards_parsed)) {
+        online[id] = true;
+        const rconTc: IRCONToolCupboard = {
+          authed_players_count: tc.Authed,
+          destroyed: false,
+          id,
+          position: tc.Position,
+        };
+        this._cache.set(id, rconTc);
       }
+  
+      // set destroyed status for TCs that are no longer on the map
+      for (const [id, tc] of this._cache) {
+        if (online[id]) continue;
+        else if (!tc.destroyed) {
+          tc.destroyed = true;
+          this._cache.set(id, tc);
+          this._logg.info(
+            "Marked TC at [%s] destroyed: entity ID %s, authed players %d",
+            tc.position.join(", "),
+            tc.id,
+            tc.authed_players_count
+          );
+        }
+      }
+    } catch (e_sync) {
+      this._logg.error(e_sync);
+      return;
     }
   }
 
