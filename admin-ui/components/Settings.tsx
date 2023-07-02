366f22b1ddebe1779efc012c3d154bcefafebc08
jalho <alho.jere@gmail.com>
2023-07-02 16:33:52 +0300

    Add gameworld samples to settings

--- admin-ui/components/Settings.tsx
+++ admin-ui/components/Settings.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { useDispatch, useSelector } from "react-redux";
 import { IAdminUIState, closeSettingsModal } from "../state/slices/local";
 import { State } from "../state/store";
+import Marker from "./Marker";
 
 const style = {
   position: "absolute" as "absolute",
@@ -18,6 +18,93 @@ const style = {
   p: 4,
 };
 
+const MAP_SAMPLE_SIZE_PX = 100;
+const SAMPLE_DATA: Array<IRCONPlayer | IRCONToolCupboard> = [
+  {
+    connected_seconds: 1,
+    country: "FI",
+    health: 100.0,
+    id: "12345678901234567",
+    ip_address: "192.168.0.123",
+    name: "player name 1",
+    online: true,
+    ping: 1,
+    position: [0, 0, 0],
+    rotation: [0, 0, 0],
+  },
+  {
+    connected_seconds: 1,
+    country: "SE",
+    health: 50.0,
+    id: "12345678901234568",
+    ip_address: "192.168.0.124",
+    name: "player name 2",
+    online: true,
+    ping: 64,
+    position: [0, 0, -15],
+    rotation: [0, 0, 0],
+  },
+  {
+    connected_seconds: 1,
+    country: "NO",
+    health: 25.0,
+    id: "12345678901234569",
+    ip_address: "192.168.0.125",
+    name: "player name 3",
+    online: false,
+    ping: 128,
+    position: [0, 0, 15],
+    rotation: [0, 0, 0],
+  },
+  {
+    connected_seconds: 1,
+    country: "DE",
+    health: 25.0,
+    id: "12345678901234560",
+    ip_address: "192.168.0.126",
+    name: "player name 4",
+    online: true,
+    ping: 256,
+    position: [-30, 0, 20],
+    rotation: [0, 0, 0],
+  },
+  {
+    authed_players_count: 1,
+    destroyed: false,
+    id: "1234567",
+    position: [-30, 0, -20],
+  },
+  {
+    authed_players_count: 10,
+    destroyed: false,
+    id: "8901234",
+    position: [30, 0, 20],
+  },
+  {
+    authed_players_count: 1,
+    destroyed: true,
+    id: "3468979",
+    position: [30, 0, -20],
+  },
+];
+
+const GameworldSample = (props: { image: string }): JSX.Element => {
+  return (
+    <div style={{ position: "relative" }}>
+      <img src={props.image} style={{ width: MAP_SAMPLE_SIZE_PX }} />
+      {SAMPLE_DATA.map((n, idx) => (
+        <Marker
+          key={"playersample" + idx}
+          data={n}
+          scale={1}
+          gameworldOrigin={MAP_SAMPLE_SIZE_PX / 2}
+          markerGameworldCoordinates={n.position}
+        />
+      ))}
+    </div>
+  );
+};
+
 export default function Settings() {
   const open = useSelector<State, IAdminUIState["settingsModalOpen"]>((s) => s.uiSettings.settingsModalOpen);
   const dispatch = useDispatch();
@@ -27,9 +114,11 @@ export default function Settings() {
     <div>
       <Modal open={open} onClose={handleClose}>
         <Box sx={style}>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            TODO: settings controls here
-          </Typography>
+          <GameworldSample image="map-samples/desert.png" />
+          <GameworldSample image="map-samples/grass.png" />
+          <GameworldSample image="map-samples/launch-site.png" />
+          <GameworldSample image="map-samples/snow.png" />
+          <GameworldSample image="map-samples/water.png" />
         </Box>
       </Modal>
     </div>
