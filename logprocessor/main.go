1bd8adea199de002bba15048fc4ca19334a909bf
jalho <alho.jere@gmail.com>
2023-07-06 21:59:14 +0300

    Add comments in Finnish

--- logprocessor/main.go
+++ logprocessor/main.go
@@ -16,11 +16,13 @@ func watchDirectory(directoryPath string, callback func(fsnotify.Event)) {
 	}
 	defer watcher.Close()
 
+	// Käy läpi hakemiston tiedostot rekursiivisesti
 	err = filepath.Walk(directoryPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
+		// Jos kyseessä ei ole hakemisto, kutsu annettua callback-funktiota
 		if !info.IsDir() {
 			callback(fsnotify.Event{
 				Name: path,
@@ -45,6 +47,7 @@ func watchDirectory(directoryPath string, callback func(fsnotify.Event)) {
 				if !ok {
 					return
 				}
+				// Kutsu annettua callback-funktiota kun tapahtuma havaitaan
 				callback(event)
 			case err, ok := <-watcher.Errors:
 				if !ok {
@@ -55,6 +58,7 @@ func watchDirectory(directoryPath string, callback func(fsnotify.Event)) {
 		}
 	}()
 
+	// Lisää hakemisto tarkkailijaan
 	err = watcher.Add(directoryPath)
 	if err != nil {
 		fmt.Printf("Error adding directory to watcher: %s\n", err)
@@ -74,6 +78,7 @@ func main() {
 
 	directoryPath := os.Args[1]
 
+	// Callback-funktio, joka tulostaa tapahtuman tiedot
 	callback := func(event fsnotify.Event) {
 		action := "created"
 		if event.Op&fsnotify.Write == fsnotify.Write {
@@ -83,5 +88,6 @@ func main() {
 		fmt.Printf("File %s was %s\n", event.Name, action)
 	}
 
+	// Käynnistä hakemiston tarkkailu
 	watchDirectory(directoryPath, callback)
 }
