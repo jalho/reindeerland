2347a2a00dd7c6019d59b56c5528c60b357df7a0
jalho <alho.jere@gmail.com>
2023-07-24 15:26:52 +0300

    Start multiple USSS instances

--- rcon-ws-proxy/src/main.rs
+++ rcon-ws-proxy/src/main.rs
@@ -3,20 +3,35 @@ use std::sync::mpsc;
 mod usss;
 
 fn main() {
-    let cfg = usss::USSSInstanceOpts {
-        upstream_command: "playerlistpos",
-        upstream_state_sync_interval_ms: 100,
-    };
-
     let (tx, rx) = mpsc::channel();
+    let tx_1 = tx;
+    let tx_2 = tx_1.clone();
 
-    // TODO: start n instances of usss, each with different args
-    usss::start(tx, cfg);
+    // TODO: remove tx from "config" -- read config from filesystem at startup
+    let configs: Vec<usss::USSSInstanceOpts> = vec![
+        usss::USSSInstanceOpts {
+            upstream_command: "playerlistpos",
+            upstream_state_sync_interval_ms: 600,
+            tx: tx_1,
+        },
+        usss::USSSInstanceOpts {
+            upstream_command: "listtoolcupboards",
+            upstream_state_sync_interval_ms: 600,
+            tx: tx_2,
+        },
+    ];
+    for config in configs {
+        usss::start(config);
+    }
 
-    // TODO: aggregate received data of all usss instances (i.e. implement isr)
+    // TODO: aggregate received data of all usss instances (see "ISR" in README)
     for received in rx {
         println!("Got: {}", received);
     }
 
-    // TODO: start m instances of dss that send the aggregated state to all connected downstreams
+    /*
+     * TODO:
+     *  start m instances of "DSS" (see README) that send the aggregated state
+     *  received from "ISR" to all connected downstreams
+     */
 }
--- rcon-ws-proxy/src/usss/mod.rs
+++ rcon-ws-proxy/src/usss/mod.rs
@@ -17,28 +17,28 @@ fn get_usss_static_opts() -> USSSStaticOpts {
 pub struct USSSInstanceOpts {
     pub upstream_command: &'static str,
     pub upstream_state_sync_interval_ms: u64,
+    pub tx: mpsc::Sender<String>,
 }
-pub fn start(tx: mpsc::Sender<String>, opts: USSSInstanceOpts) -> thread::JoinHandle<()> {
-    let h = thread::spawn(move || {
-        let usss_static_opts = get_usss_static_opts();
-        println!(
-            "TODO: send cmd '{}' to upstream '{}' using password '{}'",
-            opts.upstream_command,
-            usss_static_opts.upstream_address,
-            usss_static_opts.rcon_password
-        );
-        // TODO: get data from upstream
-        // TODO: parse data and send it via tx
-        let vals = vec![
-            String::from("more"),
-            String::from("messages"),
-            String::from("for"),
-            String::from("you"),
-        ];
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_millis(opts.upstream_state_sync_interval_ms));
-        }
-    });
+pub fn start(opts: USSSInstanceOpts) -> thread::JoinHandle<()> {
+    let h = thread::spawn(move || work(opts));
     return h;
+}
+fn work(opts: USSSInstanceOpts) {
+    let usss_static_opts: USSSStaticOpts = get_usss_static_opts();
+    println!(
+        "TODO: send cmd '{}' to upstream '{}' using password '{}'",
+        opts.upstream_command, usss_static_opts.upstream_address, usss_static_opts.rcon_password
+    );
+    // TODO: get data from upstream
+    // TODO: parse data and send it via tx
+    let vals = vec![
+        String::from("more"),
+        String::from("messages"),
+        String::from("for"),
+        String::from("you"),
+    ];
+    for val in vals {
+        opts.tx.send(val).unwrap();
+        thread::sleep(Duration::from_millis(opts.upstream_state_sync_interval_ms));
+    }
 }
