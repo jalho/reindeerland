25f724a32908a3aaecd5182b4659a070250051c2
jalho <alho.jere@gmail.com>
2023-08-21 15:33:29 +0300

    Document, use snake case

--- rcon-ws-proxy/main.cpp
+++ rcon-ws-proxy/main.cpp
@@ -4,33 +4,58 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
+/**
+ * Replace each occurrence of `placeholder` in `input` with `value`.
+ *
+ * @returns a new, substituted string and its length
+ */
+std::pair<std::string, int> subst_get_len(const std::string &input, const std::string &placeholder, const std::string &value)
+{
+    // Create a copy of the input string
+    std::string substituted_string = input;
+
+    // Find and replace all occurrences of the placeholder with the value
+    size_t pos = 0;
+    while ((pos = substituted_string.find(placeholder, pos)) != std::string::npos)
+    {
+        substituted_string.replace(pos, placeholder.length(), value);
+        pos += value.length();
+    }
+
+    // Get the length of the new string
+    int length = substituted_string.length();
+
+    // Return the new string and its length as a pair
+    return std::make_pair(substituted_string, length);
+}
+
 int main()
 {
-    int serverSocket, clientSocket;
-    struct sockaddr_in serverAddr, clientAddr;
-    socklen_t addrSize = sizeof(struct sockaddr_in);
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_size = sizeof(struct sockaddr_in);
 
     // Create a socket
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == -1)
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1)
     {
         perror("Error creating socket");
         return 1;
     }
 
     // Bind the socket to an IP address and port
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(8080); // Port 8080
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080); // Port 8080
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
     {
         perror("Error binding socket");
         return 1;
     }
 
     // Listen for incoming connections
-    if (listen(serverSocket, 5) == -1)
+    if (listen(server_socket, 5) == -1)
     {
         perror("Error listening");
         return 1;
@@ -41,8 +66,8 @@ int main()
     while (true)
     {
         // Accept a client connection
-        clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &addrSize);
-        if (clientSocket == -1)
+        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_size);
+        if (client_socket == -1)
         {
             perror("Error accepting connection");
             return 1;
@@ -52,16 +77,16 @@ int main()
 
         // Handle the HTTP request (minimal response)
         std::string response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
-        send(clientSocket, response.c_str(), response.length(), 0);
+        send(client_socket, response.c_str(), response.length(), 0);
 
         std::cout << "Client disconnected" << std::endl;
 
         // Close the client socket
-        close(clientSocket);
+        close(client_socket);
     }
 
     // Close the server socket
-    close(serverSocket);
+    close(server_socket);
 
     return 0;
 }
