14dde39a577d7c559f7dc25b5c8817a11d5b18d5
jalho <alho.jere@gmail.com>
2023-09-09 17:08:09 +0300

    Centralize error code definitions

--- rcon-ws-proxy/main.c
+++ rcon-ws-proxy/main.c
@@ -1,6 +1,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
+
+#include "rwp-errors.h"
 #include "gateway.h"
 #include "server.h"
 
@@ -11,16 +13,18 @@ int main()
 {
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd < 0)
-		return 1;
+		return RWP_SERVER_SOCK_CANNOT_CREATE;
 
 	struct sockaddr_in server_saddr = {
 		.sin_family = AF_INET,
 		.sin_port = htons(8080),
 		.sin_addr.s_addr = INADDR_ANY,
 	};
-
-	if (rwp_server_init(&server_fd, &server_saddr) < 0)
-		return 2;
+	int server_status = -1;
+	if ((server_status = rwp_server_init(&server_fd, &server_saddr)) != 0)
+	{
+		return server_status;
+	}
 
 	struct sockaddr client_saddr = {};
 	socklen_t client_saddr_sz = sizeof client_saddr;
@@ -29,7 +33,7 @@ int main()
 		printf("Waiting for new connections...\n");
 		int client_fd = accept(server_fd, &client_saddr, &client_saddr_sz);
 		if (client_fd < 0)
-			return 3;
+			return RWP_SERVER_CANNOT_ACCEPT;
 		rwp_handle_connection(&client_fd);
 	}
 	return 0;
--- rcon-ws-proxy/server.h
+++ rcon-ws-proxy/server.h
@@ -2,14 +2,12 @@
 #define RWP_SERVER_H
 
 #include <netinet/in.h>
-
-#define RWP_SERVER_CANNOT_BIND -1
-#define RWP_SERVER_CANNOT_LISTEN -2
+#include "rwp-errors.h"
 
 /**
  * Binds a given address to a given server socket.
  *
- * Returns 0 on success, and <0 on failures.
+ * Returns 0 on success.
  */
 int rwp_server_init(int *server_socket, struct sockaddr_in *server_address);
 
