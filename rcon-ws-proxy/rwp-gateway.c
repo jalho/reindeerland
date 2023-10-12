253dbdd5513c7a8c8707e03f671c62b1362ddf3f
jalho <alho.jere@gmail.com>
2023-10-13 00:38:06 +0300

    Read client sent data

--- rcon-ws-proxy/rwp-gateway.c
+++ rcon-ws-proxy/rwp-gateway.c
@@ -1,9 +1,46 @@
 #include "rwp-gateway.h"
 #include "rwp-log.h"
 
-void* rwp_handle_connection(void *client_fd)
+// TODO: Return when HTTP request is considered ended? Now waiting until FIN, i.e. never returning before client closes the connection...
+int rwp_read_http_request(int *client_fd, RWP_InboundRequest *request)
+{
+	int read_bytes = -1;
+	int read_bytes_total = 0;
+	while ((read_bytes = read(*client_fd, request->data_buf, request->data_buf_size)) != 0)
+	{
+		read_bytes_total += read_bytes;
+	}
+	return read_bytes_total;
+}
+
+void *rwp_handle_connection(void *client_fd)
 {
 	rwp_log("Handling a connection\n");
+	RWP_InboundRequest inbound_request = {0};
+	inbound_request.data_buf_size = INBOUND_REQUEST_DATA_BUF_SIZE;
+
+	int read_bytes_total = rwp_read_http_request(client_fd, &inbound_request);
+	int last_read_byte_idx = 0;
+	if (read_bytes_total > inbound_request.data_buf_size)
+	{
+		rwp_log("Missed ");
+		printf(
+			"%d bytes from client due to buffer size limit (%d bytes read total, up to %d in buffer)\n",
+			read_bytes_total - inbound_request.data_buf_size,
+			read_bytes_total,
+			inbound_request.data_buf_size);
+		last_read_byte_idx = inbound_request.data_buf_size - 1;
+	}
+	else
+		last_read_byte_idx = read_bytes_total - 1;
+	if (inbound_request.data_buf_size - (last_read_byte_idx + 1) >= 2)
+	{
+		inbound_request.data_buf[last_read_byte_idx + 1] = '\n';
+		inbound_request.data_buf[last_read_byte_idx + 2] = '\0';
+		rwp_log(inbound_request.data_buf);
+	}
+	else
+		rwp_log("Cannot log inbound request pretty -- to space for linebreak and null termination\n");
 
 	// TODO: evaluate auth
 
@@ -15,15 +52,15 @@ void* rwp_handle_connection(void *client_fd)
 
 	rwp_log("Writing something to the connected client socket...\n");
 	const char *response = "HTTP/1.1 204 No Content\r\n\r\n";
-	int written = write(*(int*)client_fd, response, strlen(response));
+	int written = write(*(int *)client_fd, response, strlen(response));
 	if (written < 0)
 		rwp_log("Write failed!\n");
 	else
 		rwp_log("Wrote successfully!\n");
 	rwp_log("Closing the client socket...\n");
-	if (close(*(int*)client_fd) < 0)
+	if (close(*(int *)client_fd) < 0)
 		rwp_log("Failed to close client socket!\n");
 	else
 		rwp_log("Client socket closed successfully!\n");
-	return (void*)NULL;
+	return (void *)NULL;
 }
--- rcon-ws-proxy/rwp-gateway.h
+++ rcon-ws-proxy/rwp-gateway.h
@@ -5,6 +5,12 @@
 #include <unistd.h>
 #include <string.h>
 
+#define INBOUND_REQUEST_DATA_BUF_SIZE 1024
+typedef struct {
+    int data_buf_size;
+    char data_buf[INBOUND_REQUEST_DATA_BUF_SIZE];
+} RWP_InboundRequest;
+
 /**
  * Handle an accepted client connection.
  */
