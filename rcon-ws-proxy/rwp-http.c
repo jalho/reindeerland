83c116ed6ca30a2b6e79e0492212366b47bf6eb3
jalho <alho.jere@gmail.com>
2023-10-13 19:03:12 +0300

    Document, add TODO comment

--- rcon-ws-proxy/rwp-http.c
+++ rcon-ws-proxy/rwp-http.c
@@ -5,9 +5,17 @@ int rwp_read_http_request(int *client_fd, RWP_InboundRequest *request)
 {
 	int read_bytes = -1;
 	int read_bytes_total = 0;
+
+	/*
+		When `client_fd` is a file descriptor of a TCP socket, `read` considers
+		end of file reached when the client has sent a packet with FIN flag,
+		i.e. intentionally gracefully closed the TCP connection. Therefore this
+		loop would end at client connection termination (when `read` returns 0).
+	*/
 	while ((read_bytes = read(*client_fd, request->data_buf, request->data_buf_size)) != 0)
 	{
 		read_bytes_total += read_bytes;
+		// TODO: break out of the loop when it can be determined that the HTTP request has been fully received
 	}
 	return read_bytes_total;
 }
