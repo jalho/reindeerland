369d1eec335545d03e9927270ff3a74c78455c67
jalho <alho.jere@gmail.com>
2023-10-22 16:02:29 +0300

    Bind TLS loadbalancer to 443

--- admin-ui/constants/upstreams.ts
+++ admin-ui/constants/upstreams.ts
@@ -1,6 +1,6 @@
 // map-service
-export const MAP_UPSTREAM = new URL("https://weekly.reindeerland.eu:28016/map");
+export const MAP_UPSTREAM = new URL("https://weekly.reindeerland.eu/map");
 
 // rcon-ws-proxy
-export const RCON_UPSTREAM_LOGIN = new URL("https://weekly.reindeerland.eu:28016/login");
-export const RCON_UPSTREAM_WS = new URL("wss://weekly.reindeerland.eu:28016/ws");
+export const RCON_UPSTREAM_LOGIN = new URL("https://weekly.reindeerland.eu/login");
+export const RCON_UPSTREAM_WS = new URL("wss://weekly.reindeerland.eu/ws");
